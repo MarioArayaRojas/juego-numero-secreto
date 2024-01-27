dc505a103db57fdeda95dd62a55665900cd34ae3
MarioArayaRojas <marioarayarojas1990@gmail.com>
2024-01-27 13:48:17 -0600

    vercion final 1.0

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-let numeroSecreto = generarNumeroSecreto();
-let intentos = 1;
-
-console.log(numeroSecreto);
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 100;
 
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
@@ -12,24 +12,64 @@ function asignarTextoElemento(elemento, texto) {
 function verificarIntento() {
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
     
-    console.log(intentos);
     if (numeroDeUsuario === numeroSecreto) {
         asignarTextoElemento('p',`Acertaste el numero secreto en ${intentos} ${(intentos === 1) ? 'intento' : 'intentos'}. FELICIDADES...`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     } else{
+        //El usuario no acerto
         if (numeroDeUsuario > numeroSecreto){
             asignarTextoElemento('p','El número secreto es menor. Vamos!.');
         } else {
             asignarTextoElemento('p','El número es mayor. Vamos!.');
         }
         intentos++;
-
+        limpiarCaja();
     }
     return;
 }
 
-function generarNumeroSecreto() {
-    return Math.floor(Math.random()*50)+1;
+function limpiarCaja() {
+   document.querySelector('#valorUsuario').value = '';
 }
 
-asignarTextoElemento('h1','Cual será el número secreto');
-asignarTextoElemento('p','Adivina un numero entre el 1 y el 50')
+function generarNumeroSecreto() {
+    let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
+
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+    //Si ya sorteamos todos los numeros
+    if (listaNumerosSorteados.length == (numeroMaximo)) {
+        asignarTextoElemento('p','Ya no quedan numeros posibles a sortear')
+    } else {
+
+        //Si el numero generado esta en la lista
+        if (listaNumerosSorteados.includes(numeroGenerado)) {
+            return generarNumeroSecreto();
+        } else {
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
+}
+
+function condicionesIniciales() {
+    asignarTextoElemento('h1','Adivina el numero Secreto!!!');
+    asignarTextoElemento('p',`Adivina un numero entre el 1 y el ${numeroMaximo}.`)
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+}
+
+function reiniciarJuego() {
+    //limpiar caja
+    limpiarCaja();
+    //indicar mensaje de intervalo de numeros
+    //generar el numero aleatorio
+    //inicializar intentos
+    condicionesIniciales();
+    //deshavilitar boton nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled','true');
+    
+}
+
+condicionesIniciales();
+
