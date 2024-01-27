5b53955ede878d701d6efea4675b2d48dd2f1593
MarioArayaRojas <marioarayarojas1990@gmail.com>
2024-01-26 19:19:10 -0600

    primeras pruebas en el app

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-alert('Hola Mundo')
+//Variables
+let numeroMaximoPosible = 10
+let numeroSecreto = Math.floor(Math.random()*numeroMaximoPosible)+1;
+let numeroDeUsuario = 0;
+let intentos = 1;
+//let palabraVeces = 'vez';
+let maximosIntentos = 3;
+
+//bucle
+while (numeroDeUsuario != numeroSecreto) {
+    numeroDeUsuario = parseInt(prompt(`Me indicas un numero entre 1 y ${numeroMaximoPosible} por favor`));
+
+    console.log(typeof(numeroDeUsuario));
+    if (numeroDeUsuario == numeroSecreto) {
+        //Acertamos, fue verdadera la condicion
+        alert(`Acertaste, el numero es: ${numeroDeUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
+    } else{
+        if (numeroDeUsuario > numeroSecreto) {
+            alert('El numero secreto es menor');
+        } else {
+            alert('El numero secreto es mayor');
+        }
+        //Incrementamos el contador cuando no acierta 
+        intentos++;
+
+        if (intentos > maximosIntentos) {
+            alert('Llegaste al numero maximo de intentos');
+            break;
+        }
+        //La condicion no se cumplio
+        //alert('Lo siento no acestaste el numero'):
+    }
+}
